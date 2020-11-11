5627aad728ae5423e53717eaa2bcae720859a9cc
akutimi <62243479+akutimi@users.noreply.github.com>
2020-11-11 16:57:57 +0200

    Update testCmmdc.py

--- testCmmdc.py
+++ testCmmdc.py
@@ -1,9 +1,9 @@
-from cmmdc import calculeaza
+from cmmdc import calcul
 
 
 if __name__=="__main__":
-  assert calculeaza(10,0)==0
-  assert calculeaza(10,5)==5
-  assert calculeaza(5,7)==1
-  assert calculeaza(12,18)==6
-  assert calculeaza(39,26)==13
+  assert calcul(10,0)==0
+  assert calcul(10,5)==5
+  assert calcul(5,7)==1
+  assert calcul(12,18)==6
+  assert calcul(39,26)==13
